a19ea51d94895e4b388556db1f0cca01eda79ae9
Evgeniy Bunya <bunya.evgeniy@gmail.com>
2020-03-30 22:35:46 +0300

    Чтение cin
    реализовано в сыроватом виде - через scanf()

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,76 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <memory>
 #include "Rectangle.h"
+#define MAX_RECTANGLES_NUMBER 100
 
+class WorkData
+{
+public:
+  WorkData()
+  {
+    // нам известно максимально допустимое количество элементов
+    // поэтому смело резервируем
+    rectangles.reserve(MAX_RECTANGLES_NUMBER);
+  }
+
+  ~WorkData()
+  {
+  }
+
+  void readInput()
+  {
+    int iteration = 0;
+    bool dataExists = true;
+    const double eps = 1e-10;
+    while (dataExists)
+    {
+      double w = 0.0;
+      double h = 0.0;
+      scanf("%lf,%lf", &w, &h);
+      // проверяем - корректны ли данные
+      // (если некорректные - завершаем цикл)
+      dataExists = (fabs(w) >=  eps) && (fabs(h) >= eps);
+      if (dataExists)
+      {
+	// первая строчка - размер контейнера
+	if (iteration == 0)
+	{
+	  pContainer = std::make_unique<Rectangle>(w, h);
+	}
+	// остальные - размеры прямоугольников
+	else
+	{
+	  rectangles.emplace_back(std::make_unique<Rectangle>(w, h));
+	}
+	iteration++;
+      }
+    }      
+  }
+  void print()
+  {
+    if (pContainer != nullptr)
+    {
+      std::cout << "container param" << std::endl;
+      std::cout << pContainer->getString() << std::endl;
+    }
+    for (auto const& item : rectangles)
+    {
+      std::cout << "rectangle param" << std::endl;
+      std::cout << item->getString() << std::endl;
+    }
+  }
+private:
+  // объявляем контейнер
+  std::unique_ptr <Rectangle> pContainer;
+  // хранилище данных о прямоугольниках
+  std::vector <std::unique_ptr <Rectangle>> rectangles;
+};
 int main()
 {
-   return 0;
+  WorkData data;
+  data.readInput();
+  data.print();
+  return 0;
 }
