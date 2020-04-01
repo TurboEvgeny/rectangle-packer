760834254aed0dd0ff5dac0e7bef018ce2e1309c
Evgeniy Bunya <bunya.evgeniy@gmail.com>
2020-04-01 20:15:51 +0300

    основная логика
    добавлена функция вставки прямоугольников

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,9 @@ public:
 	// первая строчка - размер контейнера
 	if (iteration == 0)
 	{
-	  pContainer = std::make_unique<Container>(w, h);
+	  this->containerW = w;
+	  this->containerH = h;
+	  containers.emplace_back(std::make_unique<Container>(w, h));
 	}
 	// остальные - размеры прямоугольников
 	else
@@ -50,6 +52,35 @@ public:
       }
     }      
   }
+  bool insertItemToContainer(Container* pContainer, Rectangle* pRectangle)
+  {
+    // если доступной площади нет - сразу выходим
+    if (!pRectangle->insertAreaAvailable(*pContainer));
+    {
+      return false;
+    }
+    // в ином случае итеративно пытаемся вставить элемент
+    // задаем шаг сдвига
+    const double dStep = 0.1;
+    // начальные точки
+    double xInit = 0.0;
+    double yInit = 0.0;
+    double angleInit = 0.0;
+    for (double coordX=xInit ; coordX < pContainer->getWidth() ; coordX+=dStep)
+    {
+      for (double coordY=yInit; coordY < pContainer->getHeight(); coordY+=dStep)
+      {
+        pRectangle->setCoordinates(coordX, coordY, angleInit);
+        bool result = pContainer->insertRectangle(pRectangle);
+        if (result)
+        {
+          return true;
+        }
+      }
+    }
+    // не удалось - выдаем соответсвующий результат 
+    return false;
+  }
   void packContainers()
   {
     // первым дело сортируем прямоугольники по площади
@@ -59,13 +90,52 @@ public:
 	 {
 	    return lhs->getArea() > rhs->getArea();
 	 });
-    
+    // начинаем перебирать прямоугольники
+    for (auto& pRectangle : rectangles)
+    {
+      bool insertion = false;
+      auto containersIter = containers.begin();
+      while (!insertion && (containersIter != containers.end()))
+      {
+        Container* pContainer = containersIter->get();
+        // если элемент не влезет в контейнер - просто пропускаем его
+	// (вдруг контейнеры будут разной формы)
+        if (!pRectangle->compatible(*pContainer))
+        {
+	  break;
+        }
+        else
+        {	  
+          insertion =
+	    insertItemToContainer(pContainer, pRectangle.get());
+	  ++containersIter;
+        }
+      }
+      // если не смогли вставить в существующие контейнеры -
+      // создаем новый и вставляем в него
+      if (!insertion)
+      {
+        containers.emplace_back(
+	   std::make_unique<Container>(containerW, containerH));
+        bool ok = false;
+	Container* pNewContainer = containers.back().get();
+        // проверяем на совместимость с дефолтным новым контейнером
+	if (pRectangle->compatible(*pNewContainer))
+	{
+	  ok =
+	    insertItemToContainer(pNewContainer, pRectangle.get());
+	  if (!ok)
+	    std::cout << "Error!" << std::endl;
+	}
+      }
+    }
   }
   void print()
   {
     Rectangle test(0, 0);
-    if (pContainer != nullptr)
+    if (containers.size() > 0)
     {
+      Container* pContainer = containers.front().get();
       std::cout << "container param" << std::endl;
       std::cout << "w=" << pContainer->getWidth() << " ";
       std::cout << "h=" << pContainer->getHeight() << std::endl;
@@ -76,12 +146,16 @@ public:
       std::cout << "rectangle param" << std::endl;
       std::cout << item->getString() << std::endl;
     }
+    
   }
 private:
   // объявляем контейнер
-  std::unique_ptr <Container> pContainer;
+  std::vector<std::unique_ptr <Container>> containers;
   // хранилище данных о прямоугольниках
   std::vector <std::unique_ptr <Rectangle>> rectangles;
+  // размеры контейнера
+  double containerW;
+  double containerH;
 };
 int main()
 {
