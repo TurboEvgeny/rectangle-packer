0d50020e07191acea371347e4586fa67d42f2c40
Evgeny Bunya <bunya.evgeniy@gmail.com>
2020-04-02 15:49:34 +0300

    main.cpp
    количество отступов приведено к адекватным 4 вместо 2

--- src/main.cpp
+++ src/main.cpp
@@ -10,172 +10,156 @@
 class WorkData
 {
 public:
-  WorkData()
-  {
-    // нам известно максимально допустимое количество элементов
-    // поэтому смело резервируем
-    rectangles.reserve(MAX_RECTANGLES_NUMBER);
-  }
+    WorkData():
+        containerH(0.0),
+        containerW(0.0)
+    {
+      // нам известно максимально допустимое количество элементов
+      // поэтому смело резервируем
+      rectangles.reserve(MAX_RECTANGLES_NUMBER);
+    }
 
-  ~WorkData()
-  {
-  }
+    ~WorkData()
+    {
+    }
 
-  void readInput()
-  {
-    int iteration = 0;
-    bool dataExists = true;
-    const double eps = 1e-10;
-    while (dataExists)
+    void readInput()
     {
-      double w = 0.0;
-      double h = 0.0;
-      scanf("%lf,%lf", &w, &h);
-      // проверяем - корректны ли данные
-      // (если некорректные - завершаем цикл)
-      dataExists = (fabs(w) >=  eps) && (fabs(h) >= eps);
-      if (dataExists)
-      {
-     // первая строчка - размер контейнера
-     if (iteration == 0)
-     {
-       this->containerW = w;
-       this->containerH = h;
-       containers.emplace_back(std::make_unique<Container>(w, h));
-     }
-     // остальные - размеры прямоугольников
-     else
-     {
-       rectangles.emplace_back(std::make_unique<Rectangle>(w, h));
-     }
-     iteration++;
-      }
-    }      
-  }
-  bool insertItemToContainer(Container* pContainer, Rectangle* pRectangle)
-  {
-    // если доступной площади нет - сразу выходим
-    if (!pRectangle->insertAreaAvailable(*pContainer))
-    {
-      return false;
+        int iteration = 0;
+        bool dataExists = true;
+        const double eps = 1e-10;
+        while (dataExists)
+        {
+            double w = 0.0;
+            double h = 0.0;
+            scanf("%lf,%lf", &w, &h);
+            // проверяем - корректны ли данные
+            // (если некорректные - завершаем цикл)
+            dataExists = (fabs(w) >=  eps) && (fabs(h) >= eps);
+            if (dataExists)
+            {
+            // первая строчка - размер контейнера
+                if (iteration == 0)
+                {
+                    this->containerW = w;
+                    this->containerH = h;
+                    containers.emplace_back(std::make_unique<Container>(w, h));
+                 }
+                // остальные - размеры прямоугольников
+                else
+                {
+                    rectangles.emplace_back(std::make_unique<Rectangle>(w, h));
+                }
+                iteration++;
+            }
+        }      
     }
-    // в ином случае итеративно пытаемся вставить элемент
-    // задаем шаг сдвига
-    const double dStep = 0.1;
-    // начальные точки
-    double xInit = 0.0;
-    double yInit = 0.0;
-    double angleInit = 0.0;
-    for (double coordX=xInit ; coordX < pContainer->getWidth() ; coordX+=dStep)
+    bool insertItemToContainer(Container* pContainer, Rectangle* pRectangle)
     {
-      // сверяемся - не вылезаем ли мы за пределы контейнера по оси Х 
-      bool axes_x_position_good = true;
-      bool axes_y_position_good = true;
-      if (!axes_x_position_good)
-      {
-          // если вылезаем по оси X - то нет смысла дальше двигать
-          break;
-      }
-      for (double coordY=yInit; coordY < pContainer->getHeight(); coordY+=dStep)
-      {
-        pRectangle->setCoordinates(coordX, coordY, angleInit);
-        axes_x_position_good = 
-            pRectangle->packingCheck_axesX(*pContainer);
-        axes_y_position_good = 
-            pRectangle->packingCheck_axesY(*pContainer);
-        // если вылезаем за пределы контейнера - надо прекращать цикл
-        // (двигаться вперед по оси бесполезно)
-        if (!axes_x_position_good || !axes_y_position_good)
+
+          /*
+        // если доступной площади нет - сразу выходим
+        if (!pRectangle->insertAreaAvailable(*pContainer))
         {
-            break;
+          return false;
         }
-        bool result = pContainer->insertRectangle(pRectangle);
-        if (result)
-        {
-          return true;
-        }
-      }
+            pRectangle->setCoordinates(coordX, coordY, angleInit);
+            axes_x_position_good = 
+                pRectangle->packingCheck_axesX(*pContainer);
+            axes_y_position_good = 
+                pRectangle->packingCheck_axesY(*pContainer);
+            // если вылезаем за пределы контейнера - надо прекращать цикл
+            // (двигаться вперед по оси бесполезно)
+            if (!axes_x_position_good || !axes_y_position_good)
+            {
+                break;
+            }
+            bool result = pContainer->insertRectangle(pRectangle);
+            if (result)
+            {
+              return true;
+            }
+        }*/
+        // не удалось - выдаем соответсвующий результат 
+        return false;
     }
-    // не удалось - выдаем соответсвующий результат 
-    return false;
-  }
-  void packContainers()
-  {
-    // первым дело сортируем прямоугольники по площади
-    // так как начинать вставку лучше с самых мелких
-    std::sort(rectangles.begin(), rectangles.end(),
-      [](const auto& lhs, const auto& rhs)
-      {
-         return lhs->getArea() < rhs->getArea();
-      });
-    // начинаем перебирать прямоугольники
-    for (auto& pRectangle : rectangles)
+    void packContainers()
     {
-      bool insertion = false;
-      auto containersIter = containers.begin();
-      // вставляем лишь в последний контейнер
-      // так как фигуры мы вставлем по возрастанию площади
-      // если фигура с меньше площадью не вставилась, 
-      // то значит и у нас вероятно не получится
-      if (!insertion && (containers.size() > 0))
-      {
-        Container* pContainer = containers.back().get();
-        // если элемент не влезет в контейнер - просто пропускаем его
-        // (вдруг контейнеры будут разной формы)
-        if (pRectangle->compatible(*pContainer))
+        // первым дело сортируем прямоугольники по площади
+        // так как начинать вставку лучше с самых мелких
+        std::sort(rectangles.begin(), rectangles.end(),
+          [](const auto& lhs, const auto& rhs)
+          {
+             return lhs->getArea() < rhs->getArea();
+          });
+        // начинаем перебирать прямоугольники
+        for (auto& pRectangle : rectangles)
         {
-            insertion =
-                insertItemToContainer(pContainer, pRectangle.get());
+            bool insertion = false;
+            auto containersIter = containers.begin();
+            // вставляем лишь в последний контейнер
+            // так как фигуры мы вставлем по возрастанию площади
+            // если фигура с меньше площадью не вставилась, 
+            // то значит и у нас вероятно не получится
+            if (!insertion && (containers.size() > 0))
+            {
+                Container* pContainer = containers.back().get();
+                // если элемент не влезет в контейнер - просто пропускаем его
+                // (вдруг контейнеры будут разной формы)
+                if (pRectangle->compatible(*pContainer))
+                {
+                    insertion =
+                        insertItemToContainer(pContainer, pRectangle.get());
+                }
+            }
+            // если не смогли вставить в существующие контейнеры -
+            // создаем новый и вставляем в него
+            if (!insertion)
+            {
+                containers.emplace_back(
+                std::make_unique<Container>(containerW, containerH));
+                bool insertOk = false;
+                Container* pNewContainer = containers.back().get();
+                // проверяем на совместимость с дефолтным новым контейнером
+                if (pRectangle->compatible(*pNewContainer))
+                {
+                   insertOk = insertItemToContainer(pNewContainer, pRectangle.get());
+                }
+                // не подошел - удаляем контейнер
+                if (!insertOk)
+                {
+                  containers.pop_back();
+                }
+            }
         }
-      }
-      // если не смогли вставить в существующие контейнеры -
-      // создаем новый и вставляем в него
-      if (!insertion)
-      {
-        containers.emplace_back(
-        std::make_unique<Container>(containerW, containerH));
-        bool insertOk = false;
-        Container* pNewContainer = containers.back().get();
-        // проверяем на совместимость с дефолтным новым контейнером
-        if (pRectangle->compatible(*pNewContainer))
-        {
-           insertOk = insertItemToContainer(pNewContainer, pRectangle.get());
-        }
-        // не подошел - удаляем контейнер
-        if (!insertOk)
-        {
-          containers.pop_back();
-        }
-      }
     }
-  }
-  void print()
-  {
-    std::cout << containers.size() << ",";
-    std::cout << containerW << "," << containerH << std::endl;
-    int id = 0;
-    for (auto & item : containers)
+    void print()
     {
-        item->print(id);
-        id++;
+        std::cout << containers.size() << ",";
+        std::cout << containerW << "," << containerH << std::endl;
+        int id = 0;
+        for (auto & item : containers)
+        {
+            item->print(id);
+            id++;
+        }
     }
-  }
 private:
-  // объявляем контейнер
-  std::vector<std::unique_ptr <Container>> containers;
-  // хранилище данных о прямоугольниках
-  std::vector <std::unique_ptr <Rectangle>> rectangles;
-  // размеры контейнера
-  double containerW;
-  double containerH;
+    // объявляем контейнер
+    std::vector<std::unique_ptr <Container>> containers;
+    // хранилище данных о прямоугольниках
+    std::vector <std::unique_ptr <Rectangle>> rectangles;
+    // размеры контейнера
+    double containerW;
+    double containerH;
 };
 int main()
 {
-  WorkData data;
-  data.readInput();
-  data.packContainers();
-  data.print();
-  int a = 4;
-  std::cin >> a;
-  return 0;
+    WorkData data;
+    data.readInput();
+    data.packContainers();
+    data.print();
+    int a = 4;
+    std::cin >> a;
+    return 0;
 }
